6e1bc7d7864f76cad4e5d5190c69e51d5088492d
Yuriy_Zmeev <yuriy.zmeev@mail.ru>
2022-10-22 11:22:17 +0600

    add Task_41 and Task_43

---
+++ Task_41/Program.cs
@@ -0,0 +1,35 @@
+﻿// // Задача 41: Пользователь вводит с клавиатуры M чисел.
+// // Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+Console.Write("Введите элементы(через пробел): ");
+int[] arr = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+
+int count = 0;
+for (int i = 0; i < arr.Length; i++)
+{
+    if (arr[i] > 0)
+    {
+        count++;
+    }
+}
+
+void PrintArray(int[] array)
+{
+    Console.Write(" ");
+    int lenght = array.Length;
+    int i1 = 0;
+    while (i1 < (lenght - 1))
+    {
+        Console.Write(array[i1] + ",");
+        i1++;
+    }
+    Console.Write(array[lenght - 1] + " ");
+}
+Console.WriteLine("Исходный массив:");
+
+PrintArray(arr);
+Console.WriteLine();
+Console.WriteLine($"Кол-во элементов > 0: {count}");
+
+
+
---
+++ Task_43/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых,
+// заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; 
+// значения b1, k1, b2 и k2 задаются пользователем.
+
+
+int FillArray(string message)
+{
+    Console.WriteLine(message);
+    int number = int.Parse(Console.ReadLine());
+    return number;
+}
+
+double b1 = FillArray("Введите значение b1: ");
+double k1 = FillArray("Введите значение k1: ");
+double b2 = FillArray("Введите значение b2: ");
+double k2 = FillArray("Введите значение k2: ");
+
+
+double x = (b2 - b1)/(k1 - k2);
+double y = k2 * x + b2;
+
+Console.WriteLine($"две прямые пересекутся в точке с координатами X: {x}, Y: {y}");
